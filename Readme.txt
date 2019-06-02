83de6e60a006a2dad40a2ab006184940ae2c9885
zhangbangwei <bangwei_zhang@saic-gm.com>
2019-06-02 17:16:42 +0800

    customized gRPC service RegisterAndDiscover framework, with etcd

--- Readme.txt
+++ Readme.txt
@@ -5,3 +5,14 @@ aa.go
 
 bb.go
 goroutine和context的学习
+context.WithCancel的使用
+
+
+registerAndDiscover目录
+自己动手实现gRPC框架的服务注册/发现/负载均衡
+客户端：通过etcd的Get接口和Watch接口（resolver.go和watcher.go）
+服务端：通过etcd的Put接口和KeepAlive接口（register.go）
+编译：go install studyBasic\src\registerAndDiscover
+参考文档
+https://segmentfault.com/a/1190000008672912?utm_source=tag-newest
+https://segmentfault.com/a/1190000014501241
--- src/aa/bb.go
+++ src/aa/bb.go
@@ -9,10 +9,11 @@ import (
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
-	eatNum := chiHanBao(ctx)
+	eatNum := chiHanBao(ctx) //主程序持续监听子goroutine的channel信使的通风报信
 	for n := range eatNum {
+		fmt.Println("n = ", n)
 		if n >= 10 {
-			cancel()
+			cancel() //达到主程序的底线后，主程序发号施令，cancel()子goroutine
 			break
 		}
 	}
@@ -32,7 +33,7 @@ func chiHanBao(ctx context.Context) <-chan int {
 			//time.Sleep(time.Second)
 			select {
 			case <-ctx.Done():
-				fmt.Printf("take %d seconds，eat %d hamburgers \n", t, n)
+				fmt.Printf("take %d seconds, eat %d hamburgers \n", t, n)
 				return
 			case c <- n:
 				incr := rand.Intn(5)
@@ -41,7 +42,7 @@ func chiHanBao(ctx context.Context) <-chan int {
 					n = 10
 				}
 				t++
-				fmt.Printf("I ate %d hamburgers\n", n)
+				fmt.Printf("I eat %d hamburgers\n", n)
 			}
 		}
 	}()
